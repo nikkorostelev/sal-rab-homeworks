352d62ae4ac832cf35e1e4eb4856368c90109517
nikkorostelev <nikkorostelev@gmail.com>
2022-10-19 00:44:13 +0200

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,11 +42,6 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
     let jsonData = JSON.stringify(data);
 
     return jsonData;
