6cae1bdb8b826c27eef8c327e7cf2c2b7771e1ab
nikkorostelev <nikkorostelev@gmail.com>
2022-10-19 00:46:39 +0200

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,15 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
+    
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    data.client = (name + " " + phone);
+    data.order.address = ("ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat);
+    data.order.sum = sum;
+
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
 }
